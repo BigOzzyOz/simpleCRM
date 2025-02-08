78204b5277373a0078917238b47bc250af617a38
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-02-09 00:52:22 +0100

    add loading bar

--- src/app/component/user/dialog-new-user/dialog-new-user.component.ts
+++ src/app/component/user/dialog-new-user/dialog-new-user.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { UserInterface } from '../../../interfaces/user.interface';
@@ -9,13 +9,14 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { User } from '../../../models/user.class';
 import { FsStorageService } from '../../../services/fs-storage.service';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 
 
 @Component({
   selector: 'app-dialog-new-user',
   standalone: true,
-  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatNativeDateModule, MatDatepickerModule],
+  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatNativeDateModule, MatDatepickerModule, MatProgressBarModule],
   templateUrl: './dialog-new-user.component.html',
   styleUrl: './dialog-new-user.component.scss'
 })
@@ -23,6 +24,7 @@ export class DialogNewUserComponent {
   storage = inject(FsStorageService);
   dialogRef = inject(MatDialogRef);
   user = new User();
+  loading: boolean = false;
   formCheck;
 
   constructor() {
@@ -43,6 +45,7 @@ export class DialogNewUserComponent {
   }
 
   saveUser(): void {
+    this.loading = true;
     this.user.birthday = (this.user.birthday as Date).getTime();
     console.log(this.user as UserInterface);
     this.storage.saveUser(this.user.toJSON()).then(() => this.dialogRef.close());
